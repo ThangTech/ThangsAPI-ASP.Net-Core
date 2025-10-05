936f7b30c80f1e23dc76af277020fff2476e4f9c
Van Thanggit config --global user.email nguyenvanthangktpm@gmail.comgit config --global user.name Van <nguyenvanthangktpm@gmail.com>
2025-10-05 22:11:11 +0700

    Add, edit, delete from web API

--- ThangUI/Controllers/RegionsController.cs
+++ ThangUI/Controllers/RegionsController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
+using ThangUI.Models;
 using ThangUI.Models.DTO;
 
 namespace ThangUI.Controllers
@@ -12,6 +15,8 @@ namespace ThangUI.Controllers
         {
             this.httpClientFactory = httpClientFactory;
         }
+
+        [HttpGet]
         public async Task<IActionResult> Index()
         {
             List<RegionDTO> response = new List<RegionDTO>();
@@ -20,11 +25,11 @@ namespace ThangUI.Controllers
             {
                 var client = httpClientFactory.CreateClient();
 
-                var httpResponese = await client.GetAsync("https://localhost:7263/api/region");
+                var httpResponse = await client.GetAsync("https://localhost:7263/api/region");
 
-                httpResponese.EnsureSuccessStatusCode(); //return 200
+                httpResponse.EnsureSuccessStatusCode(); //return 200
 
-                response.AddRange(await httpResponese.Content.ReadFromJsonAsync<IEnumerable<RegionDTO>>());
+                response.AddRange(await httpResponse.Content.ReadFromJsonAsync<IEnumerable<RegionDTO>>());
 
         
             }
@@ -35,5 +40,92 @@ namespace ThangUI.Controllers
             }
             return View(response);
         }
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(AddRegionViewModel model)
+        {
+            var client = httpClientFactory.CreateClient();
+
+            var httpRequestMessage = new HttpRequestMessage()
+            {
+                Method = HttpMethod.Post,
+                RequestUri = new Uri("https://localhost:7263/api/region"),
+                Content = new StringContent(JsonSerializer.Serialize(model), Encoding.UTF8, "application/json")
+
+            };
+
+            var httpResponse = await client.SendAsync(httpRequestMessage);
+            httpResponse.EnsureSuccessStatusCode();
+
+            var response = await httpResponse.Content.ReadFromJsonAsync<RegionDTO>();
+            
+            if(response is not null)
+            {
+                return RedirectToAction("Index", "Regions"); // Chuyển hướng đến index của controller regions
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(Guid id)
+        {
+            //ViewBag.Id = id;
+            var client = httpClientFactory.CreateClient();
+
+            var httpResponse = await client.GetFromJsonAsync<RegionDTO>($"https://localhost:7263/api/region/{id.ToString()}");
+
+            if(httpResponse is not null)
+            {
+                return View(httpResponse);
+            }
+            return View(null);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Edit(RegionDTO request)
+        {
+            var client = httpClientFactory.CreateClient();
+
+            var httpRequest = new HttpRequestMessage()
+            {
+                Method = HttpMethod.Put,
+                RequestUri = new Uri($"https://localhost:7263/api/region/{request.Id}"),
+                Content = new StringContent(JsonSerializer.Serialize(request), Encoding.UTF8, "application/json")
+            };
+
+            var httpResponse = await client.SendAsync(httpRequest);
+            httpResponse.EnsureSuccessStatusCode();
+
+            var response = await httpResponse.Content.ReadFromJsonAsync<RegionDTO>();
+            if(response is not null)
+            {
+                return RedirectToAction("Index", "Regions");
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(RegionDTO request)
+        {
+            try
+            {
+                var client = httpClientFactory.CreateClient();
+
+                var httpResponse = await client.DeleteAsync($"https://localhost:7263/api/region/{request.Id}");
+                httpResponse.EnsureSuccessStatusCode();
+
+                return RedirectToAction("Index", "Regions");
+            }
+            catch (Exception ex)
+            {
+            }
+            return View("Edit");
+
+        }
+
     }
 }
---
+++ ThangUI/Models/AddRegionViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ThangUI.Models
+{
+    public class AddRegionViewModel
+    {
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public string? RegionImageURL { get; set; }
+    }
+}
